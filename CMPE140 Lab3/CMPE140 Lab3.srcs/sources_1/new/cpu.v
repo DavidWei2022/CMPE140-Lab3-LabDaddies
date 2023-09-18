d56ab3825801af0553eaf6826e1bd2ff602207ec
ajay-walia <89554524+ajay-walia@users.noreply.github.com>
2023-09-18 14:28:04 -0700

    file successfully opened

---
+++ CMPE140 Lab3/CMPE140 Lab3.srcs/sources_1/new/cpu.v
@@ -0,0 +1,99 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Michelle Fang, Matthew Malvini, Ajay Walia, David Wei 
+// 
+// Create Date: 09/18/2023 01:21:20 PM
+// Module Name: cpu
+// Project Name: Lab 3 Group Project
+// Description: 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module cpu(
+    input rst_n,
+    input clk,
+    input [31:0] imem_insn, 
+    inout [31:0] dmem_data, 
+    output [31:0] imem_addr,
+    output [31:0] dmem_addr,
+    output dmem_wen
+    );
+  
+    //Fetch File
+    reg [7:0] mem [255:0];
+    initial
+        begin
+            $readmemb("././././inputs/addi_nohazard.dat", mem);
+        end
+
+    integer fd;
+    initial
+        begin
+            fd = $fopen("././././inputs/addi_nohazard.dat", "r");
+
+            if(fd)  $display("File successfully opened!");
+            else    $display("File NOT successfully opened!");
+        end
+       
+    //Fetch
+    integer fd;  
+    always @ (*)
+    begin
+    $display("hi");
+//    fd = $fopen("addi_nohazard.dat", "r");  
+//    if(fd)
+//        begin
+//        $fdisplay(fd,"hi");
+//        end
+//    else
+//        begin
+//        $fdisplay(fd,"failure to open file");
+//        end
+//    $fclose(fd); 
+    end
+    
+    always @ (posedge clk)
+    begin
+    end
+    
+    //Decode
+    always @ (*)
+    begin
+    end
+    
+    always @ (posedge clk)
+    begin
+    end
+    
+    //Execute
+    always @ (*)
+    begin
+    end
+    
+    always @ (posedge clk)
+    begin
+    end
+    
+    //Memory access
+    always @ (*)
+    begin
+    end
+    
+    always @ (posedge clk)
+    begin
+    end
+    
+    //Write back
+    always @ (*)
+    begin
+    end
+    
+    always @ (posedge clk)
+    begin
+    end
+endmodule
