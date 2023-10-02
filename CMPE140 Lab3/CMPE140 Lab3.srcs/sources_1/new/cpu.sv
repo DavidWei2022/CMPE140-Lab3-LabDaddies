a44c0510a39d67f331f419d40d2786bd881ae62a
DavidWei2022 <102999460+DavidWei2022@users.noreply.github.com>
2023-10-02 13:07:04 -0700

    decode and execute is completed
    YEAAAAAAAA BOIIIIIIS

--- CMPE140 Lab3/CMPE140 Lab3.srcs/sources_1/new/cpu.sv
+++ CMPE140 Lab3/CMPE140 Lab3.srcs/sources_1/new/cpu.sv
@@ -27,7 +27,7 @@ module cpu(input rst_n, input clk,input [31:0] imem_insn, inout [31:0] dmem_data
         begin
             for(integer i = 0; i < 28; i = i+1)
                begin
-                $display("%b",mem[i]);
+//                $display("%b",mem[i]);
                 if(i %4 ==0)
                 begin
                  data[j] = {mem[i+3], mem[i+2], mem[i+1], mem[i]};
@@ -44,39 +44,62 @@ module cpu(input rst_n, input clk,input [31:0] imem_insn, inout [31:0] dmem_data
     
 //    rom fetch(imem_insn, data);
     
-    always @ (posedge clk)
-    
-//    $display ("a is %h", data);
-    begin
-     
-    end
-    
-    //Decode
+
+   //Decode
     reg [6:0] opcode;
-    reg [6:0] funct7;
+    reg [11:0] imm;
     reg [2:0] funct3;
+    reg [4:0] rd;
+    reg [4:0] rs1;
+    reg [5:0] ALU_Control;
     
-    assign opcode=data[6:0];
+    always @ (posedge clk)
+    begin
+   
+//    for(integer i=0; i<7;i=i+1)begin
+        opcode=data[0][6:0];
+
+        rd=data[0][11:7];
+        funct3=data[0][14:12];
+    
+        rs1=data[0][19:15]; 
+        imm=data[0][31:20];
+//        end
+    end
+    
     
     always @ (*)
     begin
+    case(opcode)
        
-    
-    
+        7'b0010011: begin //I-type
+          if (funct3 == 3'b000) begin
+            ALU_Control = 6'b000000; //addi 
+            $display("the ALU control is : %b\n",ALU_Control);
+            end
+           else
+           begin
+           end
+            end
+    endcase
+
     end
-    
-    always @ (posedge clk)
-    begin
-    end
-    
-    //Execute
+    //execute    
     always @ (*)
+
     begin
+       rd = rs1+imm;
+       $display("rd is %b", rd);
     end
     
     always @ (posedge clk)
     begin
+        rd=data[0][11:7];    
+        rs1=data[0][19:15]; 
+        imm=data[0][31:20];
     end
+    
+
     
     //Memory access
     always @ (*)
