7424b9d1da65aa9859c8502ebf647b4d5260aee3
DavidWei2022 <102999460+DavidWei2022@users.noreply.github.com>
2023-10-02 13:57:51 -0700

    make another push please check!

--- CMPE140 Lab3/CMPE140 Lab3.srcs/sources_1/new/cpu.sv
+++ CMPE140 Lab3/CMPE140 Lab3.srcs/sources_1/new/cpu.sv
@@ -2,15 +2,18 @@
 module cpu(input rst_n, input clk,input [31:0] imem_insn, inout [31:0] dmem_data, 
     output [31:0] imem_addr,output [31:0] dmem_addr,output dmem_wen);
     reg [31:0] data[31:0];
-    
+    reg pc =0;
     //Fetch File
      //Fetch File
     reg [7:0] mem [255:0];
+    integer count =0;
     initial
+
         begin
             $readmemb("addi_nohazard.dat", mem);
+ 
         end
-
+    
     integer fd;
     integer j =0;
     initial
@@ -19,6 +22,7 @@ module cpu(input rst_n, input clk,input [31:0] imem_insn, inout [31:0] dmem_data
             fd = $fopen("addi_nohazard.dat", "r");
             if(fd)  $display("File successfully opened!");
             else    $display("File NOT successfully opened!");
+          
         end
 
        
@@ -46,57 +50,67 @@ module cpu(input rst_n, input clk,input [31:0] imem_insn, inout [31:0] dmem_data
     
 
    //Decode
-    reg [6:0] opcode;
-    reg [11:0] imm;
-    reg [2:0] funct3;
-    reg [4:0] rd;
-    reg [4:0] rs1;
-    reg [5:0] ALU_Control;
+    reg [7:0] opcode[6:0];
+    reg [7:0] imm[11:0];
+    reg [7:0] funct3[2:0];
+    reg [7:0] rd[4:0];
+    reg [7:0] rs1[4:0];
+    reg [7:0] ALU_Control[5:0];
     
     always @ (posedge clk)
     begin
    
-//    for(integer i=0; i<7;i=i+1)begin
-        opcode=data[0][6:0];
-
-        rd=data[0][11:7];
-        funct3=data[0][14:12];
+    for(integer i=0; i<7;i=i+1)begin
+        opcode[i]=data[i][6:0];
+        
+        rd[i]=data[i][11:7];
+        funct3[i]=data[i][14:12];
     
-        rs1=data[0][19:15]; 
-        imm=data[0][31:20];
-//        end
+        rs1[i]=data[i][19:15]; 
+        imm[i]=data[i][31:20];
+        end
     end
     
     
     always @ (*)
     begin
-    case(opcode)
-       
-        7'b0010011: begin //I-type
-          if (funct3 == 3'b000) begin
-            ALU_Control = 6'b000000; //addi 
-            $display("the ALU control is : %b\n",ALU_Control);
-            end
-           else
-           begin
-           end
-            end
-    endcase
-
+    for(int i =0;i<7;i=i+1)
+        begin
+            case(opcode[i])
+               
+                7'b0010011: begin //I-type
+                    $display("the opcode is %b",opcode[i]);
+                  if (funct3[i] == 3'b000) begin
+                    ALU_Control[i] = 6'b000000; //addi 
+                    $display("the ALU control is : %b\n",ALU_Control[i]);
+                    end
+                  
+                    end
+            endcase
+        end
     end
     //execute    
     always @ (*)
-
     begin
-       rd = rs1+imm;
-       $display("rd is %b", rd);
+        integer ii =0;
+//    for(int i =0;i<7;i=i+1)begin
+//        while(ii<7)begin
+       rd[0] = rs1[0]+imm[0];
+       $display("rd is %b", rd[0]);
+       $display("count is %d",count);
+       count = count+1;
+//       ii = ii+1;
+//       end
     end
     
     always @ (posedge clk)
     begin
-        rd=data[0][11:7];    
-        rs1=data[0][19:15]; 
-        imm=data[0][31:20];
+
+       for(integer i=0; i<7;i=i+1)begin
+        rd[i]=data[i][11:7];
+        rs1[i]=data[i][19:15]; 
+        imm[i]=data[i][31:20];
+        end
     end
     
 
